80e064f74e2a8c36fef7266f39af7ec311c4afbc
Tuong Nguyen <tuongnm2001@gmail.com>
2023-03-07 09:02:32 +0700

    count user

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+    const listUsers = useSelector(state => state.user.listUsers)
+
     return (
         <>
             <Navbar bg="light" expand="lg">
@@ -12,18 +16,15 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
+                            <NavDropdown title={`Users ${listUsers.length}`} id="basic-nav-dropdown">
+                                {
+                                    listUsers && listUsers.length > 0 &&
+                                    listUsers.map((item, index) => {
+                                        return (
+                                            <NavDropdown.Item href="#" key={`user-${index}`}>{item.email}</NavDropdown.Item>
+                                        )
+                                    })
+                                }
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
